42a350b8898c2b7a933a57a557a5ff0e5d416d96
Ziaul Haque <ziaul.haque@cars24.com>
2024-02-28 20:45:53 +0530

    webpack internals

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const paths = {
+	DIST: path.resolve(__dirname, "dist"),
+	SRC: path.resolve(__dirname, "src"),
+	JS: path.resolve(__dirname, "src/"),
+};
+// Webpack configuration
+module.exports = {
+	entry: path.join(paths.JS, "app.js"),
+	output: {
+		path: paths.DIST,
+		filename: "app.bundle.js",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(paths.SRC, "index.html"),
+		}),
+	],
+};
