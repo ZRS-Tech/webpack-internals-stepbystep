cd741754adc1e08785450316635ef7685f139158
Ziaul Haque <ziaul.haque@cars24.com>
2024-02-29 21:09:40 +0530

    babel config completed

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+const App = () => {
+	return <div> Hello there</div>;
+};
+render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const paths = {
 	DIST: path.resolve(__dirname, "dist"),
 	SRC: path.resolve(__dirname, "src"),
-	JS: path.resolve(__dirname, "src/"),
+	JS: path.resolve(__dirname, "src/js/"),
 };
 // Webpack configuration
 module.exports = {
-	entry: path.join(paths.JS, "app.js"),
+	mode: "development",
+	entry: path.join(paths.JS, "index.js"),
 	output: {
 		path: paths.DIST,
 		filename: "app.bundle.js",
@@ -17,4 +18,18 @@ module.exports = {
 			template: path.join(paths.SRC, "index.html"),
 		}),
 	],
+	// Loader configurations
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"],
+			},
+		],
+	},
+	// Enable importing JS files withut specifying their's extension
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
 };
